cc61714ed30a2e6b508703bcb4b114a9c96d426b
Felipe Santisteban <felipesantisteban99@gmail.com>
2025-08-13 15:02:28 -0300

    feature: plotting

--- main.py
+++ main.py
@@ -2,9 +2,7 @@ import gym_envs.pendulum_env as pendulum_env
 import time as time
 import test.random_episode_test as random_episode_test
 import agents.ppo_agent
-import numpy as np
-import matplotlib.pyplot
-matplotlib.use('Agg')
+import utils.plotting as plotting
 
 #agents.ppo_agent.train(max_timesteps=1000, total_timesteps=10000, render_window=True)
 
@@ -17,31 +15,7 @@ for i in range(1000):
     print(obs)
 env.close()
 
-
-obs_array = np.array(observations)
-for i in range(6):
-
-    data = obs_array[:, i]
-    steps = np.arange(len(data))
-
-    matplotlib.pyplot.figure(figsize=(8,4))
-    matplotlib.pyplot.plot(steps, data)
-    matplotlib.pyplot.xlabel("Step")
-    matplotlib.pyplot.ylabel(f"Columna {i}")
-
-    matplotlib.pyplot.grid(True)
-    matplotlib.pyplot.savefig(f"images/GRAFICA{i}.png")
-
-colores = ['r', 'g', 'b', 'c', 'm', 'y']
-matplotlib.pyplot.figure(figsize=(8,4))
-for i in range(6):
-    matplotlib.pyplot.plot(steps, obs_array[:, i], label=f"Columna {i}", color=colores[i])
-
-matplotlib.pyplot.xlabel("Step")
-matplotlib.pyplot.ylabel("Observaciones")
-matplotlib.pyplot.legend()
-matplotlib.pyplot.grid(True)
-matplotlib.pyplot.savefig("images/GRAFICA_ALL.png")
+plotting.one_graph_per_observation(observations)
 
 
 
--- utils/plotting.py
+++ utils/plotting.py
@@ -1,2 +1,30 @@
 import matplotlib
 matplotlib.use('Agg')
+import matplotlib.pyplot as plt
+import numpy as np
+colores = ['r', 'g', 'b', 'c', 'm', 'y']
+
+def one_graph_per_observation(observations):
+    obs_array = np.array(observations)
+    for i in range(obs_array.shape[1]):
+        data = obs_array[:, i]
+        steps = np.arange(len(data))
+        plt.figure(figsize=(8,4))
+        plt.plot(steps, data)
+        plt.xlabel("Tiempo (Steps)")
+        plt.ylabel(f"Columna {i}")
+        plt.grid(True)
+        plt.savefig(f"images/GRAFICA{i}.png")
+
+def all_observations_in_one_graph(observations):
+    plt.figure(figsize=(8,4))
+    obs_array = np.array(observations)
+    steps = np.arange(len(obs_array))
+    for i in range(obs_array.shape[1]):
+        plt.plot(steps, obs_array[:, i], label=f"Columna {i}", color=colores[i])
+
+    plt.xlabel("Tiempo (Steps)")
+    plt.ylabel("Observaciones")
+    plt.legend()
+    plt.grid(True)
+    plt.savefig("images/GRAFICA_ALL.png")
